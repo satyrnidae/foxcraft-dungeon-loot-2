71ad7fcd3f2c57b95937b273d24beb68284ce319
Isabel Maskrey <isabel@satyrn.dev>
2022-10-04 16:01:11 -0400

    ✨ Install mc-build in the root directory

--- mcb.ps1
+++ mcb.ps1
@@ -1,6 +1,6 @@
-npm i -g mc-build
+npm i mc-build
 while ($true) {
-    mcb
+    node ./node_modules/mc-build/lib/core/check_for_lang_updates.js
     Write-Output "MCB closed, restarting in five seconds..."
 
     Start-Sleep -Seconds 5
